7de668dc4ea3978aeea6b3d4eb7bd6252cf400e3
Abdoul 🎮 <111045134+abdoulxw3@users.noreply.github.com>
2026-09-24 07:16:42 +0000

    Remove unused Foundry package - project uses no custom Solidity contracts

--- packages/foundry/lib/hedera-forking/test/plugin/non-hedera-project/hardhat.config.js
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-
-require('@nomicfoundation/hardhat-ethers');
-const { projectTestConfig } = require('../.testConfig');
-
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-    ...projectTestConfig,
-};
